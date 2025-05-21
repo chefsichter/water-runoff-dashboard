ea25427b15c8b5c7e23d1789c044f83694112929
Felix Brändli <fbraendli@student.ethz.ch>
2025-05-21 21:26:10 +0200

    load custom_css.py for smaller font in table

--- dashboard/css/custom_css.py
+++ dashboard/css/custom_css.py
@@ -1,13 +1,10 @@
 import panel as pn
 
-def load_custom_css(script_dir):
-    if script_dir:
-        css_path = script_dir / "css" / "custom.css"
-        if css_path.exists():
-            with open(css_path, "r") as f:
-                custom_css = f.read()
-            pn.config.raw_css.append(custom_css)
-        else:
-            print(f"CSS-Datei nicht gefunden: {css_path}")
-    else:
-        print("Kein script_dir-Parameter übergeben. CSS konnte nicht geladen werden.")
+def load_custom_css():
+    # Tabulator: kleinere Schrift in Tabulator-Tabellen
+    pn.config.raw_css.append("""
+.tabulator .tabulator-cell,
+.tabulator .tabulator-col {
+  font-size: 10px !important;
+}
+""")
